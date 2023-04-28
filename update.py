4e70584c732c0334135b47a8077fd98c836af005
Štefan Baebler <stefan.baebler@gmail.com>
2023-04-28 19:14:20 +0200

    institutions.csv: put ??? to municipality field rather than city in case of geocoding problem for consistency with doctors.csv

--- update.py
+++ update.py
@@ -216,7 +216,7 @@ def add_gurs_geodata():
     dfgeo.fillna('', inplace=True)
     dfgeo['address'] = dfgeo.apply(lambda x: f'{x.street} {x.housenumber}{x.housenumberAppendix}'.strip() if x.housenumber else x.name[1], axis = 1)
     dfgeo['post'] = dfgeo.apply(lambda x: f'{x.zipCode} {x.zipName}'.strip() if x.zipCode else x.name[0], axis = 1)
-    dfgeo['city'] = dfgeo.apply(lambda x: x.city if x.city else '???', axis = 1)
+    dfgeo['municipality'] = dfgeo.apply(lambda x: x.municipality if x.municipality else '???', axis = 1)
 
     institutions = institutions.merge(dfgeo[['address','post','city','municipalityPart','municipality','lat','lon']], how = 'left', left_on = ['city','address'], right_index=True, suffixes=['_zzzs', ''])
     institutions.drop(['address_zzzs','city_zzzs'], axis='columns', inplace=True)
